f318bbfd0987700d7caa079d354e82e4a3952386
ATM Abdullah Al Mamun <atmabdullaham@gmail.com>
2026-05-20 00:13:59 +0600

    added image code credentials

--- index.js
+++ index.js
@@ -66,6 +66,39 @@ app.use(cors());
 app.use(express.json());
 
 
+// imgkit client
+const imgkitClient = new ImageKit({
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
+  publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  urlEndpoint: process.env.PUBLICURL,
+});
+
+// allow cross-origin requests
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/auth", function (req, res) {
+  // Your application logic to authenticate the user
+  // For example, you can check if the user is logged in or has the necessary permissions
+  // If the user is not authenticated, you can return an error response
+  const { token, expire, signature } =
+    imgkitClient.helper.getAuthenticationParameters();
+  res.send({
+    token,
+    expire,
+    signature,
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+  });
+});
+
+
+
 //middleware
 
 const verifyFBToken = async(req, res, next)=>{
